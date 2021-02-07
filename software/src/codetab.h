0562a3e259d924d3d486b509d0c54aefb9a6a878
asxs123 <54346276+asxs123@users.noreply.github.com>
2021-02-07 15:27:16 +0800

    Add files via upload

---
+++ software/src/codetab.h
@@ -0,0 +1,18 @@
+#ifndef _CODETAB_H  
+#define _CODETAB_H  
+  
+#define uint8_t unsigned char
+  
+/***************************16*16 Chinese, common gnd - column style - reverse output*********/  
+extern const uint8_t code Hzk[][32];
+  
+/************************************6*8************************************/  
+extern const uint8_t code F6x8[][6];  
+/****************************************8*16************************************/  
+extern const uint8_t code F8X16[];  
+
+extern const uint8_t code UI[];
+
+extern const uint8_t code table[];
+  
+#endif  
