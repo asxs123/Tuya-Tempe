cc7e986f518da90f781a09597b9ee607c23fc4f9
asxs123 <54346276+asxs123@users.noreply.github.com>
2021-02-10 12:58:55 +0800

    Add files via upload

---
+++ User_ADC.h
@@ -0,0 +1,20 @@
+#ifndef __USER_ADC_H__
+#define __USER_ADC_H__
+
+#ifdef USER_ADC_GLOBALS
+#define USER_ADC_EXT
+#else
+#define USER_ADC_EXT extern 
+#endif
+
+
+
+
+void ADC_init();
+
+int ADC_read();
+
+void Get_Voltage();
+
+#endif
+
